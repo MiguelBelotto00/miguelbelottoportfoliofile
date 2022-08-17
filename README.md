37a08ab421355ab890fb303f939c41e58d14b8c2
Miguelbelotto00 <belottomiguel.00@gmail.com>
2022-08-17 15:36:53 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# miguelbelotto00
+
+Miguel Belotto Portfolio made with Flutter
+
+## Versiones
+ - Flutter 3.0.5
+
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Miguel Belotto Portfolios',
+      theme: ThemeData(
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
